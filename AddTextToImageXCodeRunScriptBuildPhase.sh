7d27785bcfd531ddeded2daa81ac6c6fac055e80
michael_r_may <michael_r_may@myfastmail.com>
2013-06-02 00:26:11 +0100

    Added a check for the configuration around the build script such that it only runs on Debug

--- AddTextToImageXCodeRunScriptBuildPhase.sh
+++ AddTextToImageXCodeRunScriptBuildPhase.sh
@@ -1,8 +1,10 @@
-VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}"`
-echo ' adding '$VERSION
+if [ "$CONFIGURATION" == "Debug" ]; then
+    VERSION=`/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_FILE}"`
+    echo ' adding '$VERSION
 
-for ICONNAME in Icon.png Icon@2x.png Icon-72.png Icon-72@2x.png Icon-Small-50.png Icon-Small-50@2x.png Icon-Small.png Icon-Small@2x.png
-do
-echo 'adjusting icon for '${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME
-${PROJECT_DIR}/Tools/AddTextToImage ${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME $VERSION ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICONNAME
-done
+    for ICONNAME in Icon.png Icon@2x.png Icon-72.png Icon-72@2x.png Icon-Small-50.png Icon-Small-50@2x.png Icon-Small.png Icon-Small@2x.png
+    do
+        echo 'adjusting icon for '${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME
+        ${PROJECT_DIR}/Tools/AddTextToImage ${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME $VERSION ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICONNAME
+    done
+fi
