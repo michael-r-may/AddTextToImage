5eb48f629c9f6d84a8573c851b6f51b6997193d4
michael_r_may <michael_r_may@myfastmail.com>
2013-06-03 21:11:36 +0100

    New unit test files and switching on of loads of compiler warnings that are off by default.

---
+++ AddTextToImage/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  AddTextToImage
+//
+//  Created by Developer on 03/06/2013.
+//  Copyright (c) 2013 Four Apps. All rights reserved.
+//
+
+#ifndef AddTextToImage_main_h
+#define AddTextToImage_main_h
+
+
+
+#endif
---
+++ UnitTests/mainTests.m
@@ -0,0 +1,13 @@
+//
+//  mainTests.m
+//  AddTextToImage
+//
+//  Created by Developer on 03/06/2013.
+//  Copyright (c) 2013 Four Apps. All rights reserved.
+//
+
+#import "mainTests.h"
+
+@implementation mainTests
+
+@end
