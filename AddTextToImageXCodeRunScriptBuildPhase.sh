c6a56b7e001e6f22229f915730d544f344c55f6c
michael_r_may <michael_r_may@myfastmail.com>
2013-06-05 22:23:25 +0100

    Tweaked the example shell script for XCode AddTextToImageXCodeRunScriptBuildPhase.sh to quote all of the directory path environment variables so that spaces in project/target names do not break the script

--- AddTextToImageXCodeRunScriptBuildPhase.sh
+++ AddTextToImageXCodeRunScriptBuildPhase.sh
@@ -1,7 +1,6 @@
 if [ "$CONFIGURATION" == "Debug" ]; then
     for ICONNAME in Icon.png Icon@2x.png Icon-72.png Icon-72@2x.png Icon-Small-50.png Icon-Small-50@2x.png Icon-Small.png Icon-Small@2x.png
     do
-        echo 'adjusting icon for '${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME
-        ${PROJECT_DIR}/Tools/AddTextToImage ${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME ${INFOPLIST_FILE} ${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICONNAME
+       	"${PROJECT_DIR}/Tools/AddTextToImage" "${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME" "${INFOPLIST_FILE}" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICONNAME"
     done
 fi
