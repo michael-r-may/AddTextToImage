49f62b340eedf9bd8cafd0fddd610af718a17185
michael-r-may <michael_r_may@myfastmail.com>
2014-07-06 17:21:29 +0100

    Switch to supporting image asset catalogues for icons instead of the older icon files

--- AddTextToImageXCodeRunScriptBuildPhase.sh
+++ AddTextToImageXCodeRunScriptBuildPhase.sh
@@ -1,6 +1,10 @@
-if [ "$CONFIGURATION" == "Debug" ]; then
-    for ICONNAME in Icon.png Icon@2x.png Icon-72.png Icon-72@2x.png Icon-Small-50.png Icon-Small-50@2x.png Icon-Small.png Icon-Small@2x.png
-    do
-       	"${PROJECT_DIR}/Tools/AddTextToImage" "${PROJECT_DIR}/${PROJECT_NAME}/en.lproj/Images/$ICONNAME" "${INFOPLIST_FILE}" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICONNAME"
-    done
+if [ "$CONFIGURATION" == "Ad Hoc Release" ]; then
+	ICONNAMES=$(find ./Images.xcassets/AppIcon.appiconset -type f -name \*.png)
+	
+	for ICONNAME in ${ICONNAMES} 
+	do
+		ICONFILENAME=$(basename $ICONNAME)
+		echo "${PROJECT_DIR}/Tools/AddTextToImage" ${ICONNAME} "${INFOPLIST_FILE}" "${TARGET_BUILD_DIR}/${ICONFILENAME}"	
+   		"${PROJECT_DIR}/Tools/AddTextToImage" ${ICONNAME} "${INFOPLIST_FILE}" "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/${ICONFILENAME}"	
+	done
 fi
